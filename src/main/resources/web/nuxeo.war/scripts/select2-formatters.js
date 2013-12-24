59a4746c152dcb92f527a9487ba901b963b83c3e
Guillaume Renard <grenard@nuxeo.com>
2013-12-24 16:29:48 +0100

    add icon to display custom xhtml template in fancybox

--- src/main/resources/web/nuxeo.war/scripts/select2-formatters.js
+++ src/main/resources/web/nuxeo.war/scripts/select2-formatters.js
@@ -1,30 +1,31 @@
-    function myPictureFormatter(picture) {
-    var markup = "<table><tbody>";
-    markup += "<tr><td style='padding:5px;'>";
-    var views = picture.properties['picture:views'];
-    var thumbnailUrl;
-    var mediumUrl;
-    if (views) {
-      for (var i = 0; i < views.length; i++) {
-        if (views[i].tag) {
-          if (views[i].tag === 'thumb') {
-            thumbnailUrl = window.nxContextPath + "/site/automation/" + views[i].content.data;
-          } else if (views[i].tag === 'medium') {
-            mediumUrl =  window.nxContextPath + "/site/automation/" + views[i].content.data;
-          }
-
+function myPictureFormatter(picture) {
+  var markup = "<table><tbody>";
+  markup += "<tr><td style='padding:5px;'>";
+  var views = picture.properties['picture:views'];
+  var thumbnailUrl;
+  var mediumUrl;
+  if (views) {
+    for (var i = 0; i < views.length; i++) {
+      if (views[i].tag) {
+        if (views[i].tag === 'thumb') {
+          thumbnailUrl = window.nxContextPath + "/site/automation/" + views[i].content.data;
+        } else if (views[i].tag === 'medium') {
+          mediumUrl =  window.nxContextPath + "/site/automation/" + views[i].content.data;
         }
+
       }
     }
-    if (thumbnailUrl) {
-      markup += "<a class='myCssClass' href='" + mediumUrl + "'><img src='" + thumbnailUrl + "'/></a>"
-    }
-    markup += "</td><td style='padding:5px;'>";
-    var docUrl = picture.contextParameters['documentURL'];
-    markup += "<a href='/nuxeo/nxpath/default@select2Preview?docId=" + picture.uid + "' class='myCssClass2'>" + picture.title + "</a>";
-    if (picture.path) {
-      markup += "<span class='displayB detail' style='word-break:break-all;'>" + picture.path + "</span>";
-    }
-    markup += "</td></tr></tbody></table>";
-    return markup;
   }
+  if (thumbnailUrl) {
+    markup += "<a class='myCssClass' href='" + mediumUrl + "'><img src='" + thumbnailUrl + "'/></a>"
+  }
+  markup += "</td><td style='padding:5px;'>";
+  markup +=  picture.title;
+  var docUrl = picture.contextParameters['documentURL'];
+  markup += "<a href='" + window.nxContextPath + "/nxpath/" + picture.repository + "@select2Preview?docId=" + picture.uid + "' class='myCssClass2'><img src='" + window.nxContextPath + "/icons/preview.png' style='margin-left:5px;'></a>";
+  if (picture.path) {
+    markup += "<span class='displayB detail' style='word-break:break-all;'>" + picture.path + "</span>";
+  }
+  markup += "</td></tr></tbody></table>";
+  return markup;
+}
